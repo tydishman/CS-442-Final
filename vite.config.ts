d63c7e4de7926f2d191256282ee26217a5ae6354
Tyler <tdishman@nevada.unr.edu>
2025-12-08 16:13:10 -0800

    Updated Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import polyfillNode from 'rollup-plugin-polyfill-node';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), polyfillNode()],
+//   define: {
+//     "process.browser": true
+//   },
+//   resolve: {
+//       alias: [
+//       {
+//         find: './runtimeConfig',
+//         replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+//       },
+//     ]
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import polyfillNode from 'rollup-plugin-polyfill-node';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), polyfillNode()],
-  define: {
-    "process.browser": true
-  },
+  plugins: [react()],
   resolve: {
-      alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-      },
-    ]
+    dedupe: ['react', 'react-dom'] // ensures only one React instance
   }
 })
