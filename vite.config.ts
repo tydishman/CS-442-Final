f150d64394a1d45aae17c992c93d4582c55deba5
Tyler <tdishman@nevada.unr.edu>
2025-12-08 16:25:19 -0800

    Final fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import polyfillNode from 'rollup-plugin-polyfill-node';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), polyfillNode()],
-//   define: {
-//     "process.browser": true
-//   },
-//   resolve: {
-//       alias: [
-//       {
-//         find: './runtimeConfig',
-//         replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-//       },
-//     ]
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
